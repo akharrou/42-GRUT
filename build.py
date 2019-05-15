fc9b55463caf6fedfb7e4ebcb7d0c4ea2f2f0df0
Aymen Kh <idev.aymen@gmail.com>
2019-05-15 13:53:16 -0700

    Latest Update

--- build.py
+++ build.py
@@ -6,24 +6,37 @@
 #    By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/15 12:20:23 by akharrou          #+#    #+#              #
-#    Updated: 2019/05/15 13:25:55 by akharrou         ###   ########.fr        #
+#    Updated: 2019/05/15 13:51:34 by akharrou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import os
 import sys
 
 DEFAULT    = '\033[0m'
+ITALTIC    = '\033[3m'
 UNDELRINED = '\033[4m'
+
+print()
 
 try:
 
-	GRUT              = f'grut.template'
+	if (len(sys.argv) != 2):
+		print(f'usage: python3 build {UNDELRINED}42_project_name{DEFAULT}')
+		sys.exit(1)
+
+	if (os.getcwd().find('42-GRUT') == -1):
+		PREFIX = f'{os.getcwd()}/42-GRUT/'
+	else:
+		PREFIX = ''
+
+	GRUT              = f'{PREFIX}/grut.template'
 	EXTENSION         = sys.argv[1]
 	EXTENSION_FOLDER  = 'extensions'
 	GRUT_EXTENSION    = f'grut-{EXTENSION}.py'
 
 	with open(GRUT, 'r') as fd_grut:
-		with open(f'{EXTENSION_FOLDER}/{EXTENSION}.py', 'r') as fd_extension:
+		with open(f'{PREFIX}/{EXTENSION_FOLDER}/{EXTENSION}.py', 'r') as fd_extension:
 			with open(GRUT_EXTENSION, 'w') as fd_out:
 
 				grut = fd_grut.read()
@@ -34,5 +47,7 @@ try:
 
 				fd_out.write(grut)
 
-except Exception:
-	print(f'usage: python3 build {UNDELRINED}42_project_name{DEFAULT}')
+except Exception as e:
+	print(f'{UNDELRINED}\n\n🚨  Please Report the Issue ! 🚨{DEFAULT}  :: G.R.U.T -- © 2019 akharrou 😓\n\n')
+	print(f'{UNDELRINED}GRUT BUILD ISSUE:{DEFAULT} (copy paste and report (or dm me @akharrou) the issue)\n{ITALTIC}')
+	raise e

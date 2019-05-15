ab0c5f4d6ba66d212a37f217715288db68883c01
Aymen Kh <idev.aymen@gmail.com>
2019-05-15 12:16:55 -0700

    Latest Update

---
+++ Extensions/ft_ssl_md5.py
@@ -0,0 +1,56 @@
+# ================================================== #
+#
+#   GRUT Project Extension
+#
+#   Project: ft_ssl_md5
+#   Author:  akharrou
+#   Date:    15/05/2019
+#
+# ================================================== #
+
+import os
+
+FILE_A='__output_A__'
+FILE_B='__output_B__'
+
+def GRUTBody__ft_ssl_md5(launch_command_programA, launch_command_programB):
+
+	i = 0
+	total_trues = 0
+	total_args = len(arguments)
+	width = len(str(total_args))
+
+	print(f' {f"—" * (197 + width)}')
+	print(f'| {"":{width}} |  {"INPUT:":100}|   {"PROGRAM A:":35}|   {"PROGRAM B:":35}|  {"IDENTICAL":11}|')
+	print(f'|-{"-" * width}-|{"-" * 102}|{"-" * 38}|{"-" * 38}|{"-" * 13}|')
+
+	with open("{FILE_A}", 'w+') as fd_A:
+		with open("{FILE_B}", 'w+') as fd_B:
+
+			for arg in arguments:
+
+				arg = arg.strip('\n').replace('\t', '    ')
+
+				i += 1
+				os.system(f'{launch_command_programA} "{arg}" > {FILE_A}')
+				os.system(f'{launch_command_programB} "{arg}" > {FILE_B}')
+
+				fd_A.seek(0)
+				programA_output = fd_A.readline().rstrip('\n')
+
+				fd_B.seek(0)
+				programB_output = fd_B.readline().rstrip('\n')
+
+				print(f"""| {i:0{width}} |  {f'"{arg}"':100}|   {programA_output:35}|   {programB_output:35}| """, end="")
+
+				if (programA_output == programB_output):
+					print(f'  {f"[{GREEN}TRUE{DEFAULT}]":19}|')
+					total_trues += 1
+				else:
+					print(f'  {f"[{RED}FALSE{DEFAULT}]":19}|')
+
+	print(f'|{f"—" * (197 + width)}|')
+	print(f'[{total_trues} / {i}] identical outputs ‼️ ')
+
+	os.remove('{FILE_A}')
+	os.remove('{FILE_B}')
--- grut.py
+++ grut.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    grut-ft_ssl_md5.py                                 :+:      :+:    :+:    #
+#    grut.py                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/14 09:55:22 by akharrou          #+#    #+#              #
-#    Updated: 2019/05/15 12:04:41 by akharrou         ###   ########.fr        #
+#    Updated: 2019/05/15 12:16:28 by akharrou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,11 +27,8 @@ STRIPS     = BLUE
 
 # TITLES — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — #
 
-FILENAME='grut-ft_ssl_md5.py'
-EDITION='Ft_ssl_md5 Edition'
-
-FILE_A='__output_A__'
-FILE_B='__output_B__'
+FILENAME='grut.py'
+EDITION='Original Edition'
 
 # USAGE & MANUAL — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — #
 
@@ -115,49 +112,10 @@ print('')
 
 # BODY — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — #
 
-def GRUTBody__ft_ssl_md5():
+def GRUTBody__():
+	print('I am Grut.')
 
-	i = 0
-	total_trues = 0
-	total_args = len(arguments)
-	width = len(str(total_args))
-
-	print(f' {f"—" * (197 + width)}')
-	print(f'| {"":{width}} |  {"INPUT:":100}|   {"PROGRAM A:":35}|   {"PROGRAM B:":35}|  {"IDENTICAL":11}|')
-	print(f'|-{"-" * width}-|{"-" * 102}|{"-" * 38}|{"-" * 38}|{"-" * 13}|')
-
-	with open("{FILE_A}", 'w+') as fd_A:
-		with open("{FILE_B}", 'w+') as fd_B:
-
-			for arg in arguments:
-
-				arg = arg.strip('\n').replace('\t', '    ')
-
-				i += 1
-				os.system(f'{launch_command_programA} "{arg}" > {FILE_A}')
-				os.system(f'{launch_command_programB} "{arg}" > {FILE_B}')
-
-				fd_A.seek(0)
-				programA_output = fd_A.readline().rstrip('\n')
-
-				fd_B.seek(0)
-				programB_output = fd_B.readline().rstrip('\n')
-
-				print(f"""| {i:0{width}} |  {f'"{arg}"':100}|   {programA_output:35}|   {programB_output:35}| """, end="")
-
-				if (programA_output == programB_output):
-					print(f'  {f"[{GREEN}TRUE{DEFAULT}]":19}|')
-					total_trues += 1
-				else:
-					print(f'  {f"[{RED}FALSE{DEFAULT}]":19}|')
-
-	print(f'|{f"—" * (197 + width)}|')
-	print(f'[{total_trues} / {i}] identical outputs ‼️ ')
-
-	os.remove('{FILE_A}')
-	os.remove('{FILE_B}')
-
-GRUTBody__ft_ssl_md5(launch_command_programA, launch_command_programB)
+GRUTBody__()
 
 # FOOTER — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — #
 
